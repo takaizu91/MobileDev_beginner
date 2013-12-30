e26eb7325a269fbc0ec3cb4abd3a322c0af5bc54
Shinku <takaizu91@gmail.com>
2013-12-30 16:02:27 +0800

    Conditional Statement

---
+++ FP512/Lab2k(ii).php
@@ -0,0 +1,23 @@
+<?php
+
+	$country = "US";
+	
+	if ($country == "UK")
+	{
+		$capital = "London";
+	}
+	else if ($country == "US")
+	{
+		$capital = "Washington";
+	}
+	elseif ($country == "Malaysia")
+	{
+		$capital = "Kuala Lumpur";
+	}
+	else
+	{
+		$capital = "Heaven or Hell";
+	}
+	
+	print $country;
+?>
