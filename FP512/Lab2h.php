34b31a203586896342174e9f57bb55c0e717f8a5
Shinku <takaizu91@gmail.com>
2013-12-30 15:32:37 +0800

    Auto-Increment and Auto-Decrement Operators

---
+++ FP512/Lab2h.php
@@ -0,0 +1,29 @@
+<?php
+	
+	//define $total as 10
+	$total = 10;
+	echo $total;
+	echo "<br />";
+	
+	//increment it	
+	$total++;
+	
+	// $total now is should be 11
+	echo $total;
+	echo "<br /><br />";
+?>
+
+<?php
+	
+	//define $total as 10
+	$total = 10;
+	echo $total;
+	echo "<br />";
+	
+	//decrement it
+	$total --;
+	
+	//total should be 9
+	echo $total;
+	echo "<br />";
+?>
