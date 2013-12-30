26e1725ef14beb40f9b05f784abb35f2b0d39004
Shinku <takaizu91@gmail.com>
2013-12-30 16:09:15 +0800

    Conditional Statement (switch)

---
+++ FP512/Lab2k(iii).php
@@ -0,0 +1,22 @@
+<?php
+
+	$country = "Malaysia";
+	
+	switch ($country)
+	{
+		case "UK":
+			$capital = "London";
+			break;
+		case "US":
+			$capital = "Washington";
+			break;
+		case "Malaysia":
+			$capital = "Kuala Lumpur";
+			break;
+		default:
+			$capital = "Heaven or Hell";
+			break;
+	}
+	
+	print $country;
+?>
