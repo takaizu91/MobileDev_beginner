af967c9bfbb3560edd96b659665e9b973bf6ce38
Shinku <takaizu91@gmail.com>
2013-12-30 16:04:06 +0800

    Conditional Statement (temperature)

---
+++ FP512/Lab2k(i).php
@@ -0,0 +1,21 @@
+<?php
+
+	$temp = 150;
+	if ($temp >= 100)
+	{
+		echo "Very Hot!";
+	}
+?>
+
+<?php
+
+	$temp = 80;
+	if ($temp >= 100)
+	{
+		echo "Very Hot!";
+	}
+	else
+	{
+		echo "Within Tolerable Limits";
+	}
+?>
